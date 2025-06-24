f4e4a291589c1eab8442bbff211a33cf525abdbe
Facundo Avalos <101229814+BILBODEVE@users.noreply.github.com>
2025-06-23 22:24:45 -0300

    agregue constructor + get/set para Provincia

--- src/main/java/entidades/Localidad.java
+++ src/main/java/entidades/Localidad.java
@@ -3,6 +3,7 @@ package entidades;
 public class Localidad {
 	private int id;
 	private String nombre;
+	Provincia provincia;
 
 	public Localidad() {
 	}
@@ -10,6 +11,12 @@ public class Localidad {
 	public Localidad(int id, String nombre) {
 		this.id = id;
 		this.nombre = nombre;
+	}
+	
+	public Localidad(int id, String nombre, Provincia provincia) {
+		this.id = id;
+		this.nombre = nombre;
+		this.provincia = provincia;
 	}
 
 	public int getId() {
@@ -27,4 +34,18 @@ public class Localidad {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	public Provincia getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(Provincia provincia) {
+		this.provincia = provincia;
+	}
+
+	@Override
+	public String toString() {
+		return "Localidad [id=" + id + ", nombre=" + nombre + ", provincia=" + provincia + "]";
+	}
+	
 }
