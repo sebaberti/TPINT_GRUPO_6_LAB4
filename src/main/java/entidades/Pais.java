235dcfcd5b29229b7df1ab49919f0d8fa139f7aa
Facundo Avalos <101229814+BILBODEVE@users.noreply.github.com>
2025-06-23 22:22:49 -0300

    agregue constructores

--- src/main/java/entidades/Pais.java
+++ src/main/java/entidades/Pais.java
@@ -4,6 +4,14 @@ public class Pais {
 	  private int id;
 	  private String nombre;
 	  
+	public Pais(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public Pais() {}
+	
 	public int getId() {
 		return id;
 	}
