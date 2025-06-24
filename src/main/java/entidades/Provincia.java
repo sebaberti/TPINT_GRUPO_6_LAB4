09d86187ba27eb94d9294459e26ed8628ca7d1f3
Facundo Avalos <101229814+BILBODEVE@users.noreply.github.com>
2025-06-23 22:22:08 -0300

    implementacion de clase

---
+++ src/main/java/entidades/Provincia.java
@@ -0,0 +1,39 @@
+package entidades;
+
+public class Provincia {
+	int id;
+	String nombre;
+	Pais pais;
+	
+	public Provincia(int id, String nombre, Pais pais) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	@Override
+	public String toString() {
+		return "Provincia [id=" + id + ", nombre=" + nombre + ", pais=" + pais + "]";
+	}
+	
+}
