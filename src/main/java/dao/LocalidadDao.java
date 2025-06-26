6484be208351eaa15a2eb5dbbdad0e6df5a89253
Facundo Avalos <101229814+BILBODEVE@users.noreply.github.com>
2025-06-26 01:21:32 -0300

    agregue listarLoclaidades() y obtenerPorID()

--- src/main/java/dao/LocalidadDao.java
+++ src/main/java/dao/LocalidadDao.java
@@ -3,7 +3,10 @@ package dao;
 import java.util.List;
 
 import entidades.Localidad;
+import entidades.Provincia;
 
 public interface LocalidadDao {
 	 public List<Localidad> listarLocalidadesBuenosAires();
+	 public List<Localidad> listarLocalidades();
+	 public Localidad obtenerLocalidadPorID(int id);
 }
