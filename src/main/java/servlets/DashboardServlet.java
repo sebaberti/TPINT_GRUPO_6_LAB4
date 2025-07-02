1f98dc6333cc2dec3eb7a40ec9539aefdc7c75e0
Agus <142065648+agusdeansodev@users.noreply.github.com>
2025-07-02 10:07:25 -0300

    DashboardServlet.java

--- src/main/java/servlets/DashboardServlet.java
+++ src/main/java/servlets/DashboardServlet.java
@@ -2,49 +2,49 @@ package servlets;
 
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import dao.CuentaDao;
-import dao.MovimientoDao;
-import dao.CuotaDao;
-import daoImplementacion.CuotaDaoImplementacion;
-import daoImplementacion.CuentaDaoImplementacion;
-import daoImplementacion.MovimientoDaoImplementacion;
+import entidades.Movimiento;
+import negocio.CuentaNegocio;
+import negocioImplementacion.CuentaNegocioImplementacion;
+import negocio.MovimientoNegocio;
+import negocioImplementacion.MovimientoNegocioImplementacion;
 
 @WebServlet("/DashboardServlet")
 public class DashboardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	CuentaNegocio cuentaNegocio = new CuentaNegocioImplementacion();
+	MovimientoNegocio movNegocio=new MovimientoNegocioImplementacion();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		CuentaDao cuentaDAO = new CuentaDaoImplementacion();
-		MovimientoDao movimientoDAO = new MovimientoDaoImplementacion();
-		CuotaDao cuotaDAO = new CuotaDaoImplementacion();
-
-		int cuotasPagas = cuotaDAO.contarCuotasPagas();
-		int cuotasImpagas = cuotaDAO.contarCuotasImpagas();
-
-
-		int totalCuentas = cuentaDAO.contarCuentas();
-		double totalIngresos = movimientoDAO.sumarMovimientosPorTipo("INGRESO");
-		double totalEgresos = movimientoDAO.sumarMovimientosPorTipo("EGRESO");
-		double saldosImpagos = cuotaDAO.sumarSaldosImpagos();
+		 String fechaDesde = request.getParameter("fechaDesde");
+		 String fechaHasta = request.getParameter("fechaHasta");
+		 
+		 LocalDate desde = LocalDate.parse(fechaDesde);
+		 LocalDate hasta = LocalDate.parse(fechaHasta);
+		 
+		 int totalCuentas = cuentaNegocio.contarCuentas(desde, hasta);
+		 int totalClientes= cuentaNegocio.cantidadClientesxPeriodo(desde, hasta);
+		 
+		 double promedioCuentasXcliente=cuentaNegocio.promedioCuentasXCliente(desde, hasta);
 		
-		NumberFormat formatoMoneda = NumberFormat.getCurrencyInstance(new Locale("es", "AR"));
-		String saldosImpagosFormateado = formatoMoneda.format(saldosImpagos);
-
-
-		request.setAttribute("saldosImpagos", saldosImpagosFormateado);
-		request.setAttribute("cuotasPagas", cuotasPagas);
-		request.setAttribute("cuotasImpagas", cuotasImpagas);
 		request.setAttribute("totalCuentas", totalCuentas);
-		request.setAttribute("totalIngresos", totalIngresos);
-		request.setAttribute("totalEgresos", totalEgresos);
-
+		request.setAttribute("totalClientes", totalClientes);
+		request.setAttribute("promedioCuentasXcliente",promedioCuentasXcliente);
+		
 		request.getRequestDispatcher("/vistas/Admin/Reportes/reportes.jsp").forward(request, response);
 	}
+		
 }
+
