5f3ba659885e512e5980164e48183c121b57e27e
Agus <142065648+agusdeansodev@users.noreply.github.com>
2025-07-02 23:01:00 -0300

    implementacion fx reportes MovimientoNegocioImplementacion.java

--- src/main/java/negocioImplementacion/MovimientoNegocioImplementacion.java
+++ src/main/java/negocioImplementacion/MovimientoNegocioImplementacion.java
@@ -3,7 +3,7 @@ package negocioImplementacion;
 import daoImplementacion.MovimientoDaoImplementacion;
 import entidades.Movimiento;
 import negocio.MovimientoNegocio;
-
+import java.time.LocalDate;
 import java.util.List;
 
 public class MovimientoNegocioImplementacion implements MovimientoNegocio {
@@ -19,4 +19,16 @@ public class MovimientoNegocioImplementacion implements MovimientoNegocio {
     public List<Movimiento> obtenerMovimientosPorCuenta(int cuentaId) {
         return movDao.listarMovimientosPorCuenta(cuentaId);
     }
+
+    @Override
+	public double obtenerIngresos(LocalDate desde, LocalDate hasta) {
+		
+		return movDao.obtenerTotalIngresos(desde, hasta);
+	}
+
+	@Override
+	public double obtenerEgresos(LocalDate desde, LocalDate hasta) {
+		
+		return movDao.obtenerTotalEgresos(desde, hasta);
+	}
 }
